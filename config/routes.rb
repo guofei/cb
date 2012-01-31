d422748e6c6c5262b42db66ea5c7416a8903c83b
Guo Fei <guofei1986@gmail.com>
2012-02-01 01:11:07 +0900

    install device

--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,7 @@ CampusBox::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => "commodities#index"
 
   # See how all your routes lay out with "rake routes"
 
