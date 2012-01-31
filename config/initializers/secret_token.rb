27d5864dbd2f697901f7975fa935f90407abb944
Guo Fei <guofei1986@gmail.com>
2012-01-31 13:00:52 +0900

    campusbox init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CampusBox::Application.config.secret_token = '2cd201faf948cbe5658bfa8ae36656b255206e41343e5f97f1f89ab55f89864f43ccdfc83dab8e16c73c400312659ffa88b3dd1d6d4cd99bfd0538436a9a1599'
