4ea5f025b54a5905c67fd73b4a2ebe00eea82b84
Guo Fei <guofei1986@gmail.com>
2012-02-15 01:35:28 +0900

    del OrderCommodity

--- app/models/order_commodity.rb
+++
@@ -1,4 +0,0 @@
-class OrderCommodity < ActiveRecord::Base
-  belongs_to :order
-  belongs_to :commodity
-end
--- test/unit/order_commodity_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class OrderCommodityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
