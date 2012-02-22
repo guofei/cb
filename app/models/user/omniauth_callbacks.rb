f94ab9fc650f64a115b704ff066257836ef4f363
Guo Fei <guofei1986@gmail.com>
2012-02-23 01:06:09 +0900

    change css

--- app/models/user/omniauth_callbacks.rb
+++ app/models/user/omniauth_callbacks.rb
@@ -4,15 +4,28 @@ class User
 
     ["facebook","google","twitter"].each do |provider|
       define_method "find_or_create_for_#{provider}" do |response|
+        token = nil
+        if provider == "facebook"
+          token = response[:credentials][:token]
+        end
+
         uid = response["uid"]
         data = response["info"]
         if user = User.includes(:authorizations).where("authorizations.provider" => provider , "authorizations.uid" => uid).first
+          if token != nil and token != user.token
+            user.token = token
+            user.save
+          end
           user
         elsif user = User.find_by_email(data["email"])
           user.bind_service(response)
+          if token != nil and token != user.token
+            user.token = token
+            user.save
+          end
           user
         else
-          user = User.new_from_provider_data(provider,uid,data)
+          user = User.new_from_provider_data(provider,uid,data,token)
 
           if user.save(:validate => false)
             user.authorizations << Authorization.new(:provider => provider, :uid => uid )
@@ -27,8 +40,10 @@ class User
     end
 
 
-    def new_from_provider_data(provider, uid, data)
+    def new_from_provider_data(provider, uid, data, token)
       user = User.new
+      user.provider = provider
+      user.token = token
       user.email = data["email"]
       user.profile = Profile.new
       user.profile.name = data["name"]
